2f7b87a51c4d51ff449035d3d6c92df7267700a6
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-11-25 01:18:41 +0530

    printNumber Prob

---
+++ JS/printNum.js
@@ -0,0 +1,12 @@
+// // Print Number
+
+// const n = 20;
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+const n = 15;
+for (let i = 1; i <= n; i++) {
+  console.log(i);
+}
---
+++ python/printNum.py
@@ -0,0 +1,12 @@
+# print all Numbers
+
+# n = 7
+
+# for i in range (1, n + 1):
+#     print(i)
+
+
+tanish = 14
+for i in range (1, tanish+1):
+    print(i)
+
