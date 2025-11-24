f1ace15eabecf8ba3a8bab638c1b751e2454f9f8
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-11-25 00:40:35 +0530

    even-odd program

---
+++ JS/evenodd.js
@@ -0,0 +1,16 @@
+// // even or odd program
+
+// const n = 11;
+
+// if (n % 2 === 0) {
+//   console.log(n, "is Even Number");
+// } else {
+//   console.log(n, "is odd Number");
+// }
+
+const n = 10;
+if (n % 2 === 0) {
+  console.log("Even Number :", n);
+} else {
+  console.log("Odd Number :", n);
+}
---
+++ python/evenodd.py
@@ -0,0 +1,13 @@
+# # even odd Number
+
+# n = 12
+# if n % 2 == 0:
+#     print(n , "is Even Number")
+# else:
+#     print(n , "is Odd Number")
+
+
+n = 15
+if n % 2 == 0:
+    print ("Even Number",n)
+else : print ("Odd Number",n)
