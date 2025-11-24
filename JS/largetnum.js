3cff82d0caa40b611a420d4fdfd6c5c1d35d7537
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-11-25 00:58:47 +0530

    largetNumber prob

---
+++ JS/largetnum.js
@@ -0,0 +1,30 @@
+// // larget Number
+
+// var Tanish = 10;
+// var Sharma = 25;
+// var Num = 5;
+
+// let max = Num;
+
+// if (Sharma > max) {
+//   max = Sharma;
+// }
+// if (Tanish > max) {
+//   max = Tanish;
+// }
+// console.log("Number :", max);
+
+var Chocolate = 10;
+var iceCream = 40;
+var Shirt = 100;
+
+let max = Chocolate;
+
+if (Shirt > max) {
+  max = Shirt;
+}
+if (iceCream > max) {
+  max = iceCream;
+}
+
+console.log("Larget is:", max);
---
+++ python/largetnum.py
@@ -0,0 +1,28 @@
+# # larget Number
+
+# a = 10
+# b = 11
+# c = 12
+
+# max_val = a
+
+# if b > max_val:
+#     max_val = b
+#     if c > max_val:
+#         max_val = c
+
+# print("Larget Number : ", max_val)
+
+
+tanish = 0.10
+sharma = 20.0
+kumar = 50
+
+max_val = sharma
+
+if tanish > max_val:
+    max_val = tanish
+    if kumar > max_val:
+        max_val = kumar
+
+print ("larget Number is:", max_val)
