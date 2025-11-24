9e8a08fea961333a50fdc1232f429d51cdcabd2c
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-11-25 01:38:34 +0530

    problem practice

--- JS/sumofNaturalnum.js
+++ JS/sumofNaturalnum.js
@@ -1,9 +1,16 @@
-// Problem: Given n, calculate the sum 1 + 2 + ... + n and print it.
+// // Problem: Given n, calculate the sum 1 + 2 + ... + n and print it.
 
-const n = 10;
+// const n = 10;
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum += i;
+// }
+// console.log("Sum from 1 to", n, "is", sum);
+
+const n = 15;
 let sum = 0;
-
-for (let i = 1; i <= n; i++) {
+for (let i = 0; i <= n; i++) {
   sum += i;
 }
 console.log("Sum from 1 to", n, "is", sum);
--- python/sumofNaturalnum.py
+++ python/sumofNaturalnum.py
@@ -1,9 +1,14 @@
 # Problem: Given n, calculate the sum 1 + 2 + ... + n and print it.
 
-n = 10
-sum_ = 0
+# n = 10
+# sum_ = 0
+# for i in range (1, n+1):
+#     sum_ += i
+#     print("Sum form 1 to",n,"is",sum_)
 
+
+n = 15
+sum_ =0
 for i in range (1, n+1):
     sum_ += i
-
-    print("Sum form 1 to",n,"is",sum_)
+    print ("Sum form 1 to",n,"is",sum_)
