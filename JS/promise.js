2a2f8992875fc08094cd23eba848310d15e824e6
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-12-12 13:56:46 +0530

    WIP

--- JS/promise.js
+++ JS/promise.js
@@ -16,6 +16,7 @@
 // got: 42
 // result = 43
 
+// Returning a promise inside .then() (sequencing async work)
 function fetchuser(id) {
   return new Promise((resolve) =>
     setTimeout(() => resolve({ id, name: "A" }), 100)
@@ -31,3 +32,20 @@ function fetchOrders(userId) {
 fetchuser(1)
   .then((user) => fetchOrders(user.id)) // -> return promise
   .then((orders) => console.log("orders:", orders));
+
+// .catch errors
+Promise.resolve(10)
+  .then((x) => {
+    throw new Error("Boom !!!!");
+  })
+  .then(() => console.log("this will not run"))
+  .catch((err) => console.log("caught:", err.message)); // : boom
+
+// Catching and continuing
+dosomething()
+  .then((result) => maybeFail(result))
+  .catch((err) => {
+    console.error("recovering from error :", err);
+    return fallbackValue; // continue the chain with a fallback
+  })
+  .then((val) => console.log("continues with:", val));
