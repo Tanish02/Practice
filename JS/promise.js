8995908881f0408392badaa2ab2e4c0fb5889038
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-12-12 12:49:56 +0530

    WIP

--- JS/promise.js
+++ JS/promise.js
@@ -1,13 +1,33 @@
-// Promise chain example
+// // Promise chain example
 
-const p = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(42), 100);
-});
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve(42), 100);
+// });
 
-p.then((result) => {
-  console.log("got:", result);
-});
+// p.then((result) => {
+//   console.log("got:", result);
+// });
 
-p.then((result) => result + 1)
-  .then((x) => `value is ${x}`)
-  .then((str) => console.log(str));
+// p.then((result) => result + 1)
+//   .then((x) => `value is ${x}`)
+//   .then((str) => console.log(str));
+
+// Output after 42
+// got: 42
+// result = 43
+
+function fetchuser(id) {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve({ id, name: "A" }), 100)
+  );
+}
+
+function fetchOrders(userId) {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve(["order: 1", "order: 2"]), 100)
+  );
+}
+
+fetchuser(1)
+  .then((user) => fetchOrders(user.id)) // -> return promise
+  .then((orders) => console.log("orders:", orders));
