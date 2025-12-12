8b45b79326aaf3aab5ff9d3b5350d03055dddfbf
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-12-12 14:02:49 +0530

    WIP

--- JS/promise.js
+++ JS/promise.js
@@ -17,35 +17,55 @@
 // result = 43
 
 // Returning a promise inside .then() (sequencing async work)
-function fetchuser(id) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ id, name: "A" }), 100)
-  );
-}
+// function fetchuser(id) {
+//   return new Promise((resolve) =>
+//     setTimeout(() => resolve({ id, name: "A" }), 100)
+//   );
+// }
 
-function fetchOrders(userId) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve(["order: 1", "order: 2"]), 100)
-  );
-}
+// function fetchOrders(userId) {
+//   return new Promise((resolve) =>
+//     setTimeout(() => resolve(["order: 1", "order: 2"]), 100)
+//   );
+// }
 
-fetchuser(1)
-  .then((user) => fetchOrders(user.id)) // -> return promise
-  .then((orders) => console.log("orders:", orders));
+// fetchuser(1)
+//   .then((user) => fetchOrders(user.id)) // -> return promise
+//   .then((orders) => console.log("orders:", orders));
 
-// .catch errors
-Promise.resolve(10)
-  .then((x) => {
-    throw new Error("Boom !!!!");
-  })
-  .then(() => console.log("this will not run"))
-  .catch((err) => console.log("caught:", err.message)); // : boom
+// // .catch errors
+// Promise.resolve(10)
+//   .then((x) => {
+//     throw new Error("Boom !!!!");
+//   })
+//   .then(() => console.log("this will not run"))
+//   .catch((err) => console.log("caught:", err.message)); // : boom
 
-// Catching and continuing
-dosomething()
-  .then((result) => maybeFail(result))
-  .catch((err) => {
-    console.error("recovering from error :", err);
-    return fallbackValue; // continue the chain with a fallback
-  })
-  .then((val) => console.log("continues with:", val));
+// // ------------------------------
+// // Dummy example functions
+// // ------------------------------
+// function dosomething() {
+//   return new Promise((resolve) =>
+//     setTimeout(() => resolve("initial result"), 100)
+//   );
+// }
+
+// function maybeFail(value) {
+//   return new Promise((resolve, reject) =>
+//     setTimeout(() => {
+//       if (Math.random() > 0.5) reject("Random failure!");
+//       else resolve("Success with: " + value);
+//     }, 100)
+//   );
+// }
+
+// const fallbackValue = "fallback-value";
+
+// // Catching and continuing
+// dosomething()
+//   .then((result) => maybeFail(result))
+//   .catch((err) => {
+//     console.error("recovering from error :", err);
+//     return fallbackValue; // continue the chain with a fallback
+//   })
+//   .then((val) => console.log("continues with:", val));
