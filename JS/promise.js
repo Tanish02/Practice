48fe6bce6e5cb80af0598f32cb96c255ce74995e
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-12-12 12:18:40 +0530

    WIP

--- JS/promise.js
+++ JS/promise.js
@@ -1,4 +1,4 @@
-// Promise example
+// Promise chain example
 
 const p = new Promise((resolve, reject) => {
   setTimeout(() => resolve(42), 100);
@@ -7,3 +7,7 @@ const p = new Promise((resolve, reject) => {
 p.then((result) => {
   console.log("got:", result);
 });
+
+p.then((result) => result + 1)
+  .then((x) => `value is ${x}`)
+  .then((str) => console.log(str));
